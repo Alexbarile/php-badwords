4f75d206e74b95a5baaa93c16132ffd435b20670
Alexbarile <alexbarile93fg@yahoo.it>
2023-01-27 17:43:04 +0100

    esercizio completato

--- welcome.php
+++ welcome.php
@@ -1,12 +1,31 @@
+<?php
+
+    $paragraph = $_POST['paragraph'];
+    $censored = $_POST['censored'];
+
+    // mostro a schermo il paragrafo e la lunghezza
+
+    echo $paragraph;
+    echo strlen($paragraph); 
+    echo "<br>";
+
+    // mostro a schermo il paragrafo con la censura e la lunghezza
+    
+    echo str_replace($censored, '***', $paragraph);
+    echo strlen($paragraph); 
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <h2><?php echo $paragraph." ".$censored; ?></h2>
 </body>
 </html>
